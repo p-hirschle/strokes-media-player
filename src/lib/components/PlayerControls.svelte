9ac633b305dc4979b26659444c364e203eca1dd3
phirschle <pedrohirschle@outlook.com>
2026-05-22 14:59:30 -0300

    finished

--- src/lib/components/PlayerControls.svelte
+++ src/lib/components/PlayerControls.svelte
@@ -171,6 +171,11 @@
 					<line x1="23" y1="9" x2="17" y2="15"></line>
 					<line x1="17" y1="9" x2="23" y2="15"></line>
 				</svg>
+			{:else if $player.volume < 0.5}
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="text-soft-white/60">
+					<polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+					<path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
+				</svg>
 			{:else}
 				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="text-soft-white/60">
 					<polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
